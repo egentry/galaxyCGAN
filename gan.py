9ab8d0c7dddb68f89aff5575b8962a390e803a97
Eric Gentry <gentry.e@gmail.com>
2018-03-12 13:10:40 -0700

    minor style improvements

--- gan.py
+++ gan.py
@@ -19,13 +19,17 @@ def lrelu(x, leak=0.2, name="lrelu"):
 
 
 def conv2d(input_, output_dim, k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-    name="conv2d", padding="SAME"):
+           name="conv2d", padding="SAME"):
     with tf.variable_scope(name):
         w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
-              initializer=tf.truncated_normal_initializer(stddev=stddev))
-        conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding=padding)
+                            initializer=tf.truncated_normal_initializer(stddev=stddev))
 
-        biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+        conv = tf.nn.conv2d(input_, w,
+                            strides=[1, d_h, d_w, 1],
+                            padding=padding)
+
+        biases = tf.get_variable('biases', [output_dim],
+                                 initializer=tf.constant_initializer(0.0))
         conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
 
         return conv
@@ -46,29 +50,35 @@ def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=
 
     with tf.variable_scope(scope or "Linear"):
         matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
-                 tf.random_normal_initializer(stddev=stddev))
+                                 tf.random_normal_initializer(stddev=stddev))
         bias = tf.get_variable("bias", [output_size],
-        initializer=tf.constant_initializer(bias_start))
+                               initializer=tf.constant_initializer(bias_start))
         if with_w:
             return tf.matmul(input_, matrix) + bias, matrix, bias
         else:
             return tf.matmul(input_, matrix) + bias
 
 
-def deconv2d(input_, output_shape, k_h=5, k_w=5, d_h=2, d_w=2, name="deconv2d", stddev=0.02, with_w=False):
+def deconv2d(input_, output_shape, k_h=5, k_w=5, d_h=2, d_w=2,
+             name="deconv2d", stddev=0.02, with_w=False):
     with tf.variable_scope(name):
         # filter : [height, width, output_channels, in_channels]
         w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
                             initializer=tf.random_normal_initializer(stddev=stddev))
 
         try:
-            deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape, strides=[1, d_h, d_w, 1])
+            deconv = tf.nn.conv2d_transpose(input_, w,
+                                            output_shape=output_shape,
+                                            strides=[1, d_h, d_w, 1])
 
         # Support for verisons of TensorFlow before 0.7.0
         except AttributeError:
-            deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape, strides=[1, d_h, d_w, 1])
+            deconv = tf.nn.deconv2d(input_, w,
+                                    output_shape=output_shape,
+                                    strides=[1, d_h, d_w, 1])
 
-        biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+        biases = tf.get_variable('biases', [output_shape[-1]],
+                                 initializer=tf.constant_initializer(0.0))
         deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
 
         if with_w:
@@ -83,7 +93,9 @@ class CGAN(object):
     def __init__(self, sess, epoch, batch_size, z_dim, dataset_name,
                  image_size, X_img, y_conditional,
                  y_for_visualization_samples,
-                 checkpoint_dir, result_dir, log_dir):
+                 checkpoint_dir, result_dir, log_dir,
+                 d_learning_rate=0.0002,
+                 relative_learning_rate=10):
         self.sess = sess
         self.dataset_name = dataset_name
         self.checkpoint_dir = checkpoint_dir
@@ -132,8 +144,10 @@ class CGAN(object):
             self.c_dim = self.X_img.shape[-1]  # channel dim
 
             # train
-            self.learning_rate = 0.0002
+            self.learning_rate = d_learning_rate
             self.beta1 = 0.5
+            # relative_learning_rate = g learning rate / d learning rate
+            self.relative_learning_rate = relative_learning_rate
 
             # test
             self.sample_num = 16  # number of generated images to be saved
@@ -154,10 +168,12 @@ class CGAN(object):
         with tf.variable_scope("discriminator", reuse=reuse):
 
             net = lrelu(conv2d(x, 64, 4, 4, 2, 2, name='d_conv1'))
-            net = lrelu(bn(conv2d(net, 128, 4, 4, 2, 2, name='d_conv2'), is_training=is_training, scope='d_bn2'))
+            net = lrelu(bn(conv2d(net, 128, 4, 4, 2, 2, name='d_conv2'),
+                           is_training=is_training, scope='d_bn2'))
             net = tf.reshape(net, [self.batch_size, -1])
-            net = tf.concat(axis=1, values=[net, y]) # this is custom; maybe wrong?
-            net = lrelu(bn(linear(net, 1024, scope='d_fc3'), is_training=is_training, scope='d_bn3'))
+            net = tf.concat(axis=1, values=[net, y])  # this is custom; maybe wrong?
+            net = lrelu(bn(linear(net, 1024, scope='d_fc3'),
+                           is_training=is_training, scope='d_bn3'))
             out_logit = linear(net, 1, scope='d_fc4')
             out = tf.nn.sigmoid(out_logit)
 
@@ -173,8 +189,10 @@ class CGAN(object):
 
         with tf.variable_scope("generator", reuse=reuse):
             inputs = tf.concat(axis=1, values=[z, y])
-            net = tf.nn.relu(bn(linear(inputs, 1024, scope='g_fc1'), is_training=is_training, scope='g_bn1'))
-            net = tf.nn.relu(bn(linear(net, 128 * 16 * 16, scope='g_fc2'), is_training=is_training, scope='g_bn2'))
+            net = tf.nn.relu(bn(linear(inputs, 1024, scope='g_fc1'),
+                                is_training=is_training, scope='g_bn1'))
+            net = tf.nn.relu(bn(linear(net, 128 * 16 * 16, scope='g_fc2'),
+                                is_training=is_training, scope='g_bn2'))
             net = tf.reshape(net, [self.batch_size, 16, 16, 128])
 
             post_conv_size = 32
@@ -187,7 +205,7 @@ class CGAN(object):
 
             net = tf.nn.relu(
                 bn(conv2d(
-                    tf.image.resize_bilinear(net, [pre_conv_size, pre_conv_size]),
+                    tf.image.resize_bilinear(net, [pre_conv_size]*2),
                     64,
                     k_h=kernel_size, k_w=kernel_size, d_h=1, d_w=1,
                     name="g_rc3",
@@ -207,7 +225,7 @@ class CGAN(object):
                 pre_conv_size = post_conv_size + (kernel_size-1)
 
             out = conv2d(
-                    tf.image.resize_bilinear(net, [pre_conv_size, pre_conv_size]),
+                    tf.image.resize_bilinear(net, [pre_conv_size]*2),
                     self.c_dim,
                     k_h=kernel_size, k_w=kernel_size, d_h=1, d_w=1,
                     name="g_rc4",
@@ -218,7 +236,6 @@ class CGAN(object):
 
             return out
 
-
     def build_model(self):
         # some parameters
         image_dims = [self.input_height, self.input_width, self.c_dim]
@@ -226,7 +243,8 @@ class CGAN(object):
 
         """ Graph Input """
         # images
-        self.inputs = tf.placeholder(tf.float32, [batch_size] + image_dims, name='real_images')
+        self.inputs = tf.placeholder(tf.float32, [batch_size] + image_dims,
+                                     name='real_images')
 
         # noises
         self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
@@ -234,27 +252,33 @@ class CGAN(object):
         # conditionals
         self.y = tf.placeholder(tf.float32, [None, self.y_dim], name='y')
 
-
         """ Loss Function """
 
         # output of D for real images
-        D_real, D_real_logits, _ = self.discriminator(self.inputs, self.y, is_training=True, reuse=False)
+        D_real, D_real_logits, _ = self.discriminator(self.inputs, self.y,
+                                                      is_training=True,
+                                                      reuse=False)
 
         # output of D for fake images
         G = self.generator(self.z, self.y, is_training=True, reuse=False)
-        D_fake, D_fake_logits, _ = self.discriminator(G, self.y, is_training=True, reuse=True)
+        D_fake, D_fake_logits, _ = self.discriminator(G, self.y,
+                                                      is_training=True,
+                                                      reuse=True)
 
         # get loss for discriminator
         d_loss_real = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, labels=tf.ones_like(D_real)))
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits,
+                                                    labels=tf.ones_like(D_real)))
         d_loss_fake = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.zeros_like(D_fake)))
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits,
+                                                    labels=tf.zeros_like(D_fake)))
 
         self.d_loss = d_loss_real + d_loss_fake
 
         # get loss for generator
         self.g_loss = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.ones_like(D_fake)))
+            tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits,
+                                                    labels=tf.ones_like(D_fake)))
 
         """ Training """
         # divide trainable variables into a group for D and a group for G
@@ -266,12 +290,13 @@ class CGAN(object):
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
             self.d_optim = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1) \
                              .minimize(self.d_loss, var_list=d_vars)
-            self.g_optim = tf.train.AdamOptimizer(self.learning_rate*5, beta1=self.beta1) \
+            self.g_optim = tf.train.AdamOptimizer(self.learning_rate*self.relative_learning_rate, beta1=self.beta1) \
                              .minimize(self.g_loss, var_list=g_vars)
 
         """" Testing """
         # for test
-        self.fake_images = self.generator(self.z, self.y, is_training=False, reuse=True)
+        self.fake_images = self.generator(self.z, self.y, is_training=False,
+                                          reuse=True)
 
         """ Summary """
         d_loss_real_sum = tf.summary.scalar("d_loss_real", d_loss_real)
@@ -288,14 +313,13 @@ class CGAN(object):
         # initialize all variables
         tf.global_variables_initializer().run(session=self.sess)
 
-        # graph inputs for visualize training results
-        self.sample_z = np.random.uniform(-1, 1, size=(self.batch_size, self.z_dim))
-
         # saver to save model
         self.saver = tf.train.Saver()
 
         # summary writer
-        self.writer = tf.summary.FileWriter(self.log_dir + '/' + self.model_name, self.sess.graph)
+        self.writer = tf.summary.FileWriter(os.path.join(self.log_dir,
+                                                         self.model_name),
+                                            self.sess.graph)
 
         # restore check-point if it exits
         could_load, checkpoint_counter = self.load(self.checkpoint_dir)
@@ -322,22 +346,23 @@ class CGAN(object):
 
                 # update D network
                 _, summary_str, d_loss = self.sess.run([self.d_optim, self.d_sum, self.d_loss],
-                                               feed_dict={self.inputs: batch_images,
-                                                          self.z: batch_z,
-                                                          self.y: batch_y})
+                                                       feed_dict={self.inputs: batch_images,
+                                                                  self.z: batch_z,
+                                                                  self.y: batch_y})
                 self.writer.add_summary(summary_str, counter)
 
                 # update G network
-                _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.g_loss], feed_dict={self.z: batch_z,
-                                                                                                           self.y: batch_y})
+                _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.g_loss],
+                                                       feed_dict={self.z: batch_z,
+                                                                  self.y: batch_y})
                 self.writer.add_summary(summary_str, counter)
 
                 # display training status
                 counter += 1
                 print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
                       % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss),
-                     end="",
-                     )
+                      end="",
+                      )
                 if idx != self.num_batches-1:
                     print("\r", end="")
                 else:
@@ -346,7 +371,8 @@ class CGAN(object):
                 # save training results for every 300 steps
                 if np.mod(counter, 300) == 0:
                     label = 'train_{:02d}_{:04d}'.format(epoch, idx)
-                    self.visualize_results(epoch, label, self.y_for_visualization_samples)
+                    self.visualize_results(epoch, label,
+                                           self.y_for_visualization_samples)
 
             # After an epoch, start_batch_id is set to zero
             # non-zero value is only for the first epoch after loading pre-trained model
@@ -356,7 +382,8 @@ class CGAN(object):
             self.save(self.checkpoint_dir, counter)
 
             # show temporal results
-            self.visualize_results(epoch, "test_at_end", self.y_for_visualization_samples)
+            self.visualize_results(epoch, "test_at_end",
+                                   self.y_for_visualization_samples)
 
         # save model for final step
         self.save(self.checkpoint_dir, counter)
@@ -365,7 +392,7 @@ class CGAN(object):
         z_sample = np.random.uniform(-1, 1, size=(self.batch_size, self.z_dim))
 
         samples = self.sess.run(self.fake_images, feed_dict={self.z: z_sample,
-                                                             self.y: y_sample,})
+                                                             self.y: y_sample})
 
         return samples
 
@@ -377,7 +404,7 @@ class CGAN(object):
         y_sample = np.vstack([y_values]*self.batch_size)
 
         samples = self.sess.run(self.fake_images, feed_dict={self.z: z_sample,
-                                                             self.y: y_sample,})
+                                                             self.y: y_sample})
 
         samples = samples[:self.sample_num]
 
@@ -390,29 +417,35 @@ class CGAN(object):
             self.batch_size, self.z_dim)
 
     def save(self, checkpoint_dir, step):
-        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir, self.model_name)
+        checkpoint_dir = os.path.join(checkpoint_dir,
+                                      self.model_dir,
+                                      self.model_name)
 
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
 
-        self.saver.save(self.sess, os.path.join(checkpoint_dir, self.model_name+'.model'), global_step=step)
+        self.saver.save(self.sess,
+                        os.path.join(checkpoint_dir, self.model_name+'.model'),
+                        global_step=step)
 
     def load(self, checkpoint_dir):
         import re
         print(" [*] Reading checkpoints...")
-        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir, self.model_name)
+        checkpoint_dir = os.path.join(checkpoint_dir,
+                                      self.model_dir,
+                                      self.model_name)
 
         ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
         if ckpt and ckpt.model_checkpoint_path:
             ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
-            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
-            counter = int(next(re.finditer("(\d+)(?!.*\d)",ckpt_name)).group(0))
+            self.saver.restore(self.sess,
+                               os.path.join(checkpoint_dir, ckpt_name))
+            counter = int(next(re.finditer("(\d+)(?!.*\d)", ckpt_name)).group(0))
             print(" [*] Success to read {}".format(ckpt_name))
             return True, counter
         else:
             print(" [*] Failed to find a checkpoint")
             return False, 0
-
 
     def plot(self, samples):
         """ assumes samples is of shape [n_img, img_x, img_y, num_channels]
@@ -430,7 +463,9 @@ class CGAN(object):
             ax.set_yticklabels([])
             ax.set_aspect('equal')
             plt.imshow(transform_0_1(
-                sample.reshape(self.output_height, self.output_width, self.c_dim)
+                sample.reshape(self.output_height,
+                               self.output_width,
+                               self.c_dim)
             ))
         return fig
 
